d446a123a3e11876844335eec62fe016e7dcbef9
banderGlitch <nernaykumar98@gmail.com>
2024-10-15 15:26:50 +0530

    first commit4

--- src/components/ActionComp/SendCrypto.jsx
+++ src/components/ActionComp/SendCrypto.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Button from '../common/Button';
 import { TransactionForm } from '../SuperCommonComp';
 import {ReadOnlyTransaction }  from './SendDonateComp';
 const SendCrypto = ({ setInput }) => {
